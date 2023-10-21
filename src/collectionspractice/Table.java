0b5c41ba7ceeae59e3bd48f921df30f068d3deb7
gopi444 <gopik9031@gmail.com>
2023-10-22 00:02:34 +0530

    latest changes

---
+++ src/collectionspractice/Table.java
@@ -0,0 +1,12 @@
+package collectionspractice;
+
+public class Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=5;
+		System.out.println(num);
+
+	}
+
+}
