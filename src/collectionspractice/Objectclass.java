a2cfae6c77885b6f8765781fed3cccfc0cec240e
gopi444 <gopik9031@gmail.com>
2023-10-22 00:50:51 +0530

    latest changes

---
+++ src/collectionspractice/Objectclass.java
@@ -0,0 +1,12 @@
+package collectionspractice;
+
+public class Objectclass {
+	String name="Gopi krishna";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Objectclass obj=new Objectclass();
+		System.out.println(obj.name);
+	}
+
+}
