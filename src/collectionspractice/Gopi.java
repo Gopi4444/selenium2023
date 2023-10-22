34bd6d7d0303615484bc595812fd77212c839c67
gopi444 <gopik9031@gmail.com>
2023-10-22 11:06:21 +0530

    latest changes

---
+++ src/collectionspractice/Gopi.java
@@ -0,0 +1,12 @@
+package collectionspractice;
+
+public class Gopi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String name1="Gopi";
+    String name2="krishna";
+    System.out.println("name=:"+name1+name2);
+	}
+
+}
